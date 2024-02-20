1d270153ef1a4a4f739c1ee6d415ef37b5b5e243
Gal1leo2 <66050977@kmitl.ac.th>
2024-02-20 08:07:20 +0700

    FINAL

--- BoardPanel.java
+++ BoardPanel.java
@@ -7,98 +7,40 @@ import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel {
 
-	/**
-	 * Minimum color component values for tiles. This is required if we
-	 * want to show both light and dark shading on our tiles.
-	 */
 	public static final int COLOR_MIN = 35;
 
-	/**
-	 * Maximum color component values for tiles. This is required if we
-	 * want to show both light and dark shading on our tiles.
-	 */
 	public static final int COLOR_MAX = 255 - COLOR_MIN;
 
-	/**
-	 * The width of the border around the game board.
-	 */
 	private static final int BORDER_WIDTH = 5;
 
-	/**
-	 * The number of columns on the board.
-	 */
 	public static final int COL_COUNT = 10;
 
-	/**
-	 * The number of visible rows on the board.
-	 */
 	private static final int VISIBLE_ROW_COUNT = 20;
 
-	/**
-	 * The number of rows that are hidden from view.
-	 */
 	private static final int HIDDEN_ROW_COUNT = 2;
 
-	/**
-	 * The total number of rows that the board contains.
-	 */
 	public static final int ROW_COUNT = VISIBLE_ROW_COUNT + HIDDEN_ROW_COUNT;
 
-	/**
-	 * The number of pixels that a tile takes up.
-	 */
 	public static final int TILE_SIZE = 24;
 
-	/**
-	 * The width of the shading on the tiles.
-	 */
 	public static final int SHADE_WIDTH = 4;
 
-	/**
-	 * The central x coordinate on the game board.
-	 */
 	private static final int CENTER_X = COL_COUNT * TILE_SIZE / 2;
 
-	/**
-	 * The central y coordinate on the game board.
-	 */
 	private static final int CENTER_Y = VISIBLE_ROW_COUNT * TILE_SIZE / 2;
 
-	/**
-	 * The total width of the panel.
-	 */
 	public static final int PANEL_WIDTH = COL_COUNT * TILE_SIZE + BORDER_WIDTH * 2;
 
-	/**
-	 * The total height of the panel.
-	 */
 	public static final int PANEL_HEIGHT = VISIBLE_ROW_COUNT * TILE_SIZE + BORDER_WIDTH * 2;
 
-	/**
-	 * The larger font to display.
-	 */
 	private static final Font LARGE_FONT = new Font("Tahoma", Font.BOLD, 16);
 
-	/**
-	 * The smaller font to display.
-	 */
 	private static final Font SMALL_FONT = new Font("Tahoma", Font.BOLD, 12);
 
-	/**
-	 * The Tetris instance.
-	 */
 	private Tetris tetris;
 
-	/**
-	 * The tiles that make up the board.
-	 */
 	private TileType[][] tiles;
 
-	/**
-	 * Crates a new GameBoard instance.
-	 * 
-	 * @param tetris The Tetris instance to use.
-	 */
 	public BoardPanel(Tetris tetris) {
 		this.tetris = tetris;
 		this.tiles = new TileType[ROW_COUNT][COL_COUNT];
@@ -107,9 +49,6 @@ public class BoardPanel extends JPanel {
 		setBackground(Color.WHITE);
 	}
 
-	/**
-	 * Resets the board and clears away any tiles.
-	 */
 	public void clear() {
 		/*
 		 * Loop through every tile index and set it's value
@@ -122,15 +61,6 @@ public class BoardPanel extends JPanel {
 		}
 	}
 
-	/**
-	 * Determines whether or not a piece can be placed at the coordinates.
-	 * 
-	 * @param type     THe type of piece to use.
-	 * @param x        The x coordinate of the piece.
-	 * @param y        The y coordinate of the piece.
-	 * @param rotation The rotation of the piece.
-	 * @return Whether or not the position is valid.
-	 */
 	public boolean isValidAndEmpty(TileType type, int x, int y, int rotation) {
 
 		if (x < -type.getLeftInset(rotation) || x + type.getDimension() - type.getRightInset(rotation) >= COL_COUNT) {
@@ -151,15 +81,6 @@ public class BoardPanel extends JPanel {
 		return true;
 	}
 
-	/**
-	 * Adds a piece to the game board. Note: Doesn't check for existing pieces,
-	 * and will overwrite them if they exist.
-	 * 
-	 * @param type     The type of piece to place.
-	 * @param x        The x coordinate of the piece.
-	 * @param y        The y coordinate of the piece.
-	 * @param rotation The rotation of the piece.
-	 */
 	public void addPiece(TileType type, int x, int y, int rotation) {
 		/*
 		 * Loop through every tile within the piece and add it
@@ -175,33 +96,21 @@ public class BoardPanel extends JPanel {
 		}
 	}
 
-	/**
-	 * Checks the board to see if any lines have been cleared, and
-	 * removes them from the game.
-	 * 
-	 * @return The number of lines that were cleared.
-	 */
-	public int checkLines() {
+	public int checkAllLine() {
 		int completedLines = 0;
 
 		for (int row = 0; row < ROW_COUNT; row++) {
-			if (checkLine(row)) {
+			if (checkspecificLine(row)) {
 				completedLines++;
 			}
 		}
 		return completedLines;
 	}
 
-	/**
-	 * Checks whether or not {@code row} is full.
-	 * 
-	 * @param line The row to check.
-	 * @return Whether or not this row is full.
-	 */
-	private boolean checkLine(int line) {
+	private boolean checkspecificLine(int line) {
 
 		for (int col = 0; col < COL_COUNT; col++) {
-			if (!isOccupied(col, line)) {
+			if (isOccupied(col, line) == false) {
 				return false;
 			}
 		}
@@ -214,35 +123,14 @@ public class BoardPanel extends JPanel {
 		return true;
 	}
 
-	/**
-	 * Checks to see if the tile is already occupied.
-	 * 
-	 * @param x The x coordinate to check.
-	 * @param y The y coordinate to check.
-	 * @return Whether or not the tile is occupied.
-	 */
 	private boolean isOccupied(int x, int y) {
 		return tiles[y][x] != null;
 	}
 
-	/**
-	 * Sets a tile located at the desired column and row.
-	 * 
-	 * @param x    The column.
-	 * @param y    The row.
-	 * @param type The value to set to the tile to.
-	 */
 	private void setTile(int x, int y, TileType type) {
 		tiles[y][x] = type;
 	}
 
-	/**
-	 * Gets a tile by it's column and row.
-	 * 
-	 * @param x The column.
-	 * @param y The row.
-	 * @return The tile.
-	 */
 	TileType getTile(int x, int y) {
 		return tiles[y][x];
 	}
@@ -251,12 +139,8 @@ public class BoardPanel extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		// This helps simplify the positioning of things.
 		g.translate(BORDER_WIDTH, BORDER_WIDTH);
 
-		/*
-		 * Draw the board differently depending on the current game state.
-		 */
 		if (tetris.isPaused()) {
 			g.setFont(LARGE_FONT);
 			g.setColor(Color.WHITE);
@@ -264,13 +148,8 @@ public class BoardPanel extends JPanel {
 			g.drawString(msg, CENTER_X - g.getFontMetrics().stringWidth(msg) / 2, CENTER_Y);
 		} else if (tetris.isNewGame() || tetris.isGameOver()) {
 			g.setFont(LARGE_FONT);
-			g.setColor(Color.WHITE);
+			g.setColor(Color.BLACK);
 
-			/*
-			 * Because both the game over and new game screens are nearly identical,
-			 * we can handle them together and just use a ternary operator to change
-			 * the messages that are displayed.
-			 */
 			String msg = tetris.isNewGame() ? "TETRIS" : "GAME OVER";
 			g.drawString(msg, CENTER_X - g.getFontMetrics().stringWidth(msg) / 2, 150);
 			g.setFont(SMALL_FONT);
@@ -285,16 +164,14 @@ public class BoardPanel extends JPanel {
 				for (int y = HIDDEN_ROW_COUNT; y < ROW_COUNT; y++) {
 					TileType tile = getTile(x, y);
 					if (tile != null) {
+
 						drawTile(tile, x * TILE_SIZE, (y - HIDDEN_ROW_COUNT) * TILE_SIZE, g);
 					}
 				}
 			}
 
 			/*
-			 * Draw the current piece. This cannot be drawn like the rest of the
-			 * pieces because it's still not part of the game board. If it were
-			 * part of the board, it would need to be removed every frame which
-			 * would just be slow and confusing.
+			 * Draw the current piece
 			 */
 			TileType type = tetris.getPieceType();
 			int pieceCol = tetris.getPieceCol();
@@ -310,26 +187,18 @@ public class BoardPanel extends JPanel {
 					}
 				}
 			}
-
 			/*
-			 * Draw the ghost (semi-transparent piece that shows where the current piece
-			 * will land). I couldn't think of
-			 * a better way to implement this so it'll have to do for now. We simply take
-			 * the current position and move
-			 * down until we hit a row that would cause a collision.
+			 * Draw Ghost
 			 */
 			Color base = type.getBaseColor();
 			base = new Color(base.getRed(), base.getGreen(), base.getBlue(), 20);
 			for (int lowest = pieceRow; lowest < ROW_COUNT; lowest++) {
-				// If no collision is detected, try the next row.
 				if (isValidAndEmpty(type, pieceCol, lowest, rotation)) {
 					continue;
 				}
 
-				// Draw the ghost one row higher than the one the collision took place at.
-				lowest--;
+				lowest -= 1;
 
-				// Draw the ghost piece.
 				for (int col = 0; col < type.getDimension(); col++) {
 					for (int row = 0; row < type.getDimension(); row++) {
 						if (lowest + row >= 2 && type.isTile(col, row, rotation)) {
@@ -341,12 +210,7 @@ public class BoardPanel extends JPanel {
 
 				break;
 			}
-
-			/*
-			 * Draw the background grid above the pieces (serves as a useful visual
-			 * for players, and makes the pieces look nicer by breaking them up.
-			 */
-			g.setColor(Color.DARK_GRAY);
+			g.setColor(Color.BLUE);
 			for (int x = 0; x < COL_COUNT; x++) {
 				for (int y = 0; y < VISIBLE_ROW_COUNT; y++) {
 					g.drawLine(0, y * TILE_SIZE, COL_COUNT * TILE_SIZE, y * TILE_SIZE);
@@ -354,61 +218,18 @@ public class BoardPanel extends JPanel {
 				}
 			}
 		}
-
-		/*
-		 * Draw the outline.
-		 */
-		g.setColor(Color.WHITE);
+		g.setColor(Color.BLACK);
 		g.drawRect(0, 0, TILE_SIZE * COL_COUNT, TILE_SIZE * VISIBLE_ROW_COUNT);
 	}
 
-	/**
-	 * Draws a tile onto the board.
-	 * 
-	 * @param type The type of tile to draw.
-	 * @param x    The column.
-	 * @param y    The row.
-	 * @param g    The graphics object.
-	 */
 	private void drawTile(TileType type, int x, int y, Graphics g) {
 		drawTile(type.getBaseColor(), type.getLightColor(), type.getDarkColor(), x, y, g);
 	}
 
-	/**
-	 * Draws a tile onto the board.
-	 * 
-	 * @param base The base color of tile.
-	 * @para m light The light color of the tile.
-	 * @param dark The dark color of the tile.
-	 * @param x    The column.
-	 * @param y    The row.
-	 * @param g    The graphics object.
-	 */
 	private void drawTile(Color base, Color light, Color dark, int x, int y, Graphics g) {
 
-		/*
-		 * Fill the entire tile with the base color.
-		 */
 		g.setColor(base);
 		g.fillRect(x, y, TILE_SIZE, TILE_SIZE);
-
-		/*
-		 * Fill the bottom and right edges of the tile with the dark shading color.
-		 */
-		g.setColor(dark);
-		g.fillRect(x, y + TILE_SIZE - SHADE_WIDTH, TILE_SIZE, SHADE_WIDTH);
-		g.fillRect(x + TILE_SIZE - SHADE_WIDTH, y, SHADE_WIDTH, TILE_SIZE);
-
-		/*
-		 * Fill the top and left edges with the light shading. We draw a single line
-		 * for each row or column rather than a rectangle so that we can draw a nice
-		 * looking diagonal where the light and dark shading meet.
-		 */
-		g.setColor(light);
-		for (int i = 0; i < SHADE_WIDTH; i++) {
-			g.drawLine(x, y + i, x + TILE_SIZE - i - 1, y + i);
-			g.drawLine(x + i, y, x + i, y + TILE_SIZE - i - 1);
-		}
 	}
 
 }
--- Tetris.java
+++ Tetris.java
@@ -160,11 +160,12 @@ public class Tetris extends JFrame {
 			currentRow++;
 		} else {
 			board.addPiece(currentType, currentCol, currentRow, currentRotation);
-			int cleared = board.checkLines();
+
+			int cleared = board.checkAllLine();
 			if (cleared > 0) {
 				score += 50 << cleared;
 				gameSpeed += 2;
-				playMusic("asset/success.wav");
+				// playMusic("asset/success.wav");
 			}
 			gameSpeed += 0.25f;
 			logicTimer.setCyclesPerSecond(gameSpeed);
@@ -182,10 +183,6 @@ public class Tetris extends JFrame {
 		side.repaint();
 	}
 
-	/**
-	 * Resets the game variables to their default values at the start
-	 * of a new game.
-	 */
 	private void resetOrPLAYGame() {
 		switch (level) {
 			case 0:
@@ -212,10 +209,6 @@ public class Tetris extends JFrame {
 		spawnPiece();
 	}
 
-	/**
-	 * Spawns a new piece and resets our piece's variables to their default
-	 * values.
-	 */
 	private void spawnPiece() {
 
 		this.currentType = nextType;
@@ -224,64 +217,34 @@ public class Tetris extends JFrame {
 		this.currentRotation = 0;
 		this.nextType = TileType.values()[random.nextInt(TYPE_COUNT)];
 
-		if (!board.isValidAndEmpty(currentType, currentCol, currentRow, currentRotation)) {
+		if (board.isValidAndEmpty(currentType, currentCol, currentRow, currentRotation) == false) {
 			this.isGameOver = true;
 			logicTimer.setPaused(true);
 		}
 	}
 
-	/**
-	 * Attempts to set the rotation of the current piece to newRotation.
-	 * 
-	 * @param newRotation The rotation of the new peice.
-	 */
 	private void rotatePiece(int newRotation) {
-		/*
-		 * Sometimes pieces will need to be moved when rotated to avoid clipping
-		 * out of the board (the I piece is a good example of this). Here we store
-		 * a temporary row and column in case we need to move the tile as well.
-		 */
+
 		int newColumn = currentCol;
 		int newRow = currentRow;
 
-		/*
-		 * Get the insets for each of the sides. These are used to determine how
-		 * many empty rows or columns there are on a given side.
-		 */
 		int left = currentType.getLeftInset(newRotation);
 		int right = currentType.getRightInset(newRotation);
 		int top = currentType.getTopInset(newRotation);
 		int bottom = currentType.getBottomInset(newRotation);
 
-		/*
-		 * If the current piece is too far to the left or right, move the piece away
-		 * from the edges
-		 * so that the piece doesn't clip out of the map and automatically become
-		 * invalid.
-		 */
 		if (currentCol < -left) {
 			newColumn -= currentCol - left;
 		} else if (currentCol + currentType.getDimension() - right >= BoardPanel.COL_COUNT) {
 			newColumn -= (currentCol + currentType.getDimension() - right) - BoardPanel.COL_COUNT + 1;
 		}
 
-		/*
-		 * If the current piece is too far to the top or bottom, move the piece away
-		 * from the edges
-		 * so that the piece doesn't clip out of the map and automatically become
-		 * invalid.
-		 */
 		if (currentRow < -top) {
 			newRow -= currentRow - top;
 		} else if (currentRow + currentType.getDimension() - bottom >= BoardPanel.ROW_COUNT) {
 			newRow -= (currentRow + currentType.getDimension() - bottom) - BoardPanel.ROW_COUNT + 1;
 		}
 
-		/*
-		 * Check to see if the new position is acceptable. If it is, update the rotation
-		 * and
-		 * position of the piece.
-		 */
 		if (board.isValidAndEmpty(currentType, newColumn, newRow, newRotation)) {
 			currentRotation = newRotation;
 			currentRow = newRow;
@@ -289,47 +252,22 @@ public class Tetris extends JFrame {
 		}
 	}
 
-	/**
-	 * Checks to see whether or not the game is paused.
-	 * 
-	 * @return Whether or not the game is paused.
-	 */
 	public boolean isPaused() {
 		return isPaused;
 	}
 
-	/**
-	 * Checks to see whether or not the game is over.
-	 * 
-	 * @return Whether or not the game is over.
-	 */
 	public boolean isGameOver() {
 		return isGameOver;
 	}
 
-	/**
-	 * Checks to see whether or not we're on a new game.
-	 * 
-	 * @return Whether or not this is a new game.
-	 */
 	public boolean isNewGame() {
 		return isNewGame;
 	}
 
-	/**
-	 * Gets the current score.
-	 * 
-	 * @return The score.
-	 */
 	public int getScore() {
 		return score;
 	}
 
-	/**
-	 * Gets the current level.
-	 * 
-	 * @return The level.
-	 */
 	public int getLevel() {
 		return level;
 	}
@@ -338,38 +276,18 @@ public class Tetris extends JFrame {
 		return gameSpeed;
 	}
 
-	/**
-	 * Gets the current type of piece we're using.
-	 * 
-	 * @return The piece type.
-	 */
 	public TileType getPieceType() {
 		return currentType;
 	}
 
-	/**
-	 * Gets the next type of piece we're using.
-	 * 
-	 * @return The next piece.
-	 */
 	public TileType getNextPieceType() {
 		return nextType;
 	}
 
-	/**
-	 * Gets the column of the current piece.
-	 * 
-	 * @return The column.
-	 */
 	public int getPieceCol() {
 		return currentCol;
 	}
 
-	/**
-	 * Gets the row of the current piece.
-	 * 
-	 * @return The row.
-	 */
 	public int getPieceRow() {
 		return currentRow;
 	}
@@ -378,36 +296,26 @@ public class Tetris extends JFrame {
 		return currentPieceColor;
 	}
 
-	// Method to set the color of the current piece
 	public void setCurrentPieceColor(Color color) {
 		this.currentPieceColor = color;
 	}
 
-	/**
-	 * Gets the rotation of the current piece.
-	 * 
-	 * @return The rotation.
-	 */
 	public int getPieceRotation() {
 		return currentRotation;
 	}
 
 	public void playMusic(String filePath) {
 		try {
-			// Open an audio input stream from the specified file path
 			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filePath));
 
-			// Get a Clip object to play the audio
 			Clip clip = AudioSystem.getClip();
 
-			// Open the audio clip
 			clip.open(audioInputStream);
 			FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 			gainControl.setValue(-20.0f);
 			clip.start();
 		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-			// Handle any exceptions gracefully
-			e.printStackTrace(); // Consider logging the exception or displaying an error message
+			e.printStackTrace();
 		}
 
 	}
@@ -423,29 +331,25 @@ public class Tetris extends JFrame {
 		JFrame menuFrame = new JFrame("Tetrimino Menu");
 		MenuPanel menuPanel = new MenuPanel();
 		setResizable(false);
-		menuPanel.setLayout(null); // Use null layout for absolute positioning
+		menuPanel.setLayout(null);
 
-		// Add transparent buttons
 		JButton startButton = createTransparentButton("START");
 		JButton levelButton = createTransparentButton("LEVEL");
 
-		// Set the position and size of the buttons
 		startButton.setBounds(540, 250, 200, 100); // x, y, width, height
 		levelButton.setBounds(540, 370, 200, 100); // x, y, width, height
 
-		// Add action listeners to the buttons
 		startButton.addActionListener(e -> {
-			menuFrame.setVisible(false); // Hide the menu window
-			menuFrame.dispose(); // Dispose of the menu window resources
-			requestFocus(); // Request focus on the main game window
-			startGame(); // Start the game
+			menuFrame.setVisible(false);
+			menuFrame.dispose();
+			requestFocus();
+			startGame();
 		});
 
 		levelButton.addActionListener(e -> {
-			menuPanel.setCurrentState("Level"); // Update the state to "Level"
-			menuPanel.removeAll(); // Remove all components from the panel
+			menuPanel.setCurrentState("Level");
+			menuPanel.removeAll();
 
-			// Add level buttons with specific coordinates and size
 			menuPanel.add(createLevelButton(1, 525, 200, 250, 60, menuFrame)); // Level 1 button
 			menuPanel.add(createLevelButton(2, 555, 300, 150, 60, menuFrame)); // Level 2 button
 			menuPanel.add(createLevelButton(3, 450, 415, 400, 60, menuFrame)); // Level 3 button
@@ -453,18 +357,15 @@ public class Tetris extends JFrame {
 			menuFrame.revalidate(); // Revalidate the frame to reflect changes
 		});
 
-		// Add buttons to the menuPanel
 		menuPanel.add(startButton);
 		menuPanel.add(levelButton);
 		setResizable(false);
 
-		// Add the menuPanel to the menuFrame
 		menuFrame.add(menuPanel);
-		menuFrame.setSize(1280, 720); // Set a larger size for the menu frame
+		menuFrame.setSize(1280, 720);
 		menuFrame.setLocationRelativeTo(null);
 		menuFrame.setVisible(true);
 
-		// Set default button for the menuFrame
 		menuFrame.getRootPane().setDefaultButton(startButton);
 		setResizable(false);
 
@@ -472,25 +373,25 @@ public class Tetris extends JFrame {
 
 	public JButton createLevelButton(int level, int x, int y, int width, int height, JFrame menuFrame) {
 		JButton button = new JButton("Level " + level);
-		button.setBounds(x, y, width, height); // Set the bounds with specific coordinates and size
-		button.setOpaque(false); // Make the button transparent
-		button.setContentAreaFilled(false); // Make the content area transparent
-		button.setBorderPainted(false); // Remove border
-		button.setForeground(new Color(0, 0, 0, 0)); // Set text color to fully transparent
+		button.setBounds(x, y, width, height);
+		button.setOpaque(false);
+		button.setContentAreaFilled(false);
+		button.setBorderPainted(false);
+		button.setForeground(new Color(0, 0, 0, 0));
 		button.setFocusPainted(false); // Remove focus indication
 		button.addActionListener(e -> {
 			this.level = level; // Set the selected level
-			menuFrame.setVisible(false); // Hide the menu window
-			menuFrame.dispose(); // Dispose of the menu window resources
-			requestFocus(); // Request focus on the main game window
-			startGame(); // Start the game with the selected level
+			menuFrame.setVisible(false);
+			menuFrame.dispose();
+			requestFocus();
+			startGame();
 
 		});
 		return button;
 	}
 
 	public class MenuPanel extends JPanel {
-		private String currentState = "Start"; // Variable to track the current state
+		private String currentState = "Start";
 
 		@Override
 		protected void paintComponent(Graphics g) {
@@ -500,20 +401,19 @@ public class Tetris extends JFrame {
 			g.drawImage(imageIcon.getImage(), 0, 0, getWidth(), getHeight(), this);
 		}
 
-		// Method to update the current state
 		public void setCurrentState(String state) {
 			currentState = state;
-			repaint(); // Repaint the panel to update the wallpaper
+			repaint();
 		}
 	}
 
 	private JButton createTransparentButton(String label) {
 		JButton button = new JButton(label);
-		button.setOpaque(false); // Make the button transparent
-		button.setContentAreaFilled(false); // Make the content area transparent
-		button.setBorderPainted(false); // Remove border
-		button.setForeground(new Color(0, 0, 0, 0)); // Set text color to fully transparent
-		button.setFocusPainted(false); // Remove focus indication
+		button.setOpaque(false);
+		button.setContentAreaFilled(false);
+		button.setBorderPainted(false);
+		button.setForeground(new Color(0, 0, 0, 0));
+		button.setFocusPainted(false);
 
 		return button;
 	}
